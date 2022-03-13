8e126a2a07fe95ada8ec373147677604b334def8
Mingkai Dong <mk@dong.mk>
2022-03-13 15:02:15 +0800

    update config

---
+++ periphery/hammerspoon-clocking.lua
@@ -0,0 +1,52 @@
+-- From https://appsonthemove.com/posts/orgmode_clocking_hammerspoon/
+
+local clockingLog = hs.logger.new("clocking")
+local clockingMenu = hs.menubar.new()
+local currentTask = nil
+
+local function trim(s)
+   return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+local function eval(sexp, callback)
+   hs.task.new(
+      "/usr/local/bin/emacsclient",
+      function(exitCode, stdOut, stdErr)
+         if exitCode == 0 then
+            callback(trim(stdOut))
+         end
+      end,
+      { "--eval", sexp }
+   ):start()
+end
+
+local function updateClockingMenu()
+   eval(
+      "(org-clock-is-active)",
+      function(value)
+         if value == "nil" then
+            clockingMenu:setTitle("No Task")
+         else
+            eval(
+                "(org-clock-get-clock-string)",
+                function(value)
+                   clockingMenu:setTitle(string.match(value, '"(.+)"'))
+                end
+            )
+         end
+      end
+   )
+end
+
+local function startUpdatingClockingMenu()
+   return hs.timer.doEvery(10, updateClockingMenu)
+end
+
+local mod = {}
+
+function mod.init()
+    updateClockingMenu()
+    return startUpdatingClockingMenu()
+end
+
+return mod
