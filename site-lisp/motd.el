59c11bfc8f34583ef3ed0d3fb6c837ad9906fe2d
Mingkai Dong <mk@dong.mk>
2022-06-14 10:49:10 +0800

    drop posframe which could cause input method issues

--- site-lisp/motd.el
+++ site-lisp/motd.el
@@ -13,7 +13,8 @@
   :group 'motd)
 
 (defvar motd--today nil)
-(defvar motd--posframe-buffer " *motd-message*")
+(defvar motd--buffer-name " *motd-message*")
+(defvar motd--popup-frame nil)
 (defvar motd--git-commit-dir nil)
 
 (defun motd--git-commit ()
@@ -73,37 +74,69 @@ Recent Submission Deadlines:
                       ("Love You 😘 .x..." . t)) t)
   )
 
-(defun motd--popup-posframe (text)
-  "Show TEXT in the posframe popup."
-  (with-current-buffer (get-buffer-create motd--posframe-buffer)
+(defun motd--create-frame (buffer)
+  "Create a floating popup frame with BUFFER as content."
+  (let*  ((parent (window-frame))
+          (f (make-frame `((parent-frame . ,parent)
+                           (undecorated . 1)
+                           (name . "*Motd Frame*")
+                           (border-width . 5)
+                           (internal-border-width . 10)
+                           (minibuffer . ,(minibuffer-window parent))
+                           (left-fringe . 0)
+                           (right-fringe . 0)
+                           (menu-bar-lines . 0)
+                           (tool-bar-lines . 0)
+                           (tab-bar-lines . 0)
+                           (top-bar-lines . 0)
+                           (mode-line-format . nil)
+                           (header-line-format . nil)
+                           )))
+          (win (frame-root-window f)))
+    (set-frame-size f
+                    (/ (* (frame-inner-width) 4) 5)
+                    (/ (frame-inner-height) 3)
+                    t)
+    (set-frame-position f
+                        (/ (frame-inner-width) 10)
+                        (/ (frame-inner-height) 6))
+    (set-frame-parameter f 'background-color "lightgreen")
+    (set-frame-parameter f 'border-color "grey")
+    (set-window-buffer win buffer)
+    (set-window-dedicated-p win t)
+  f
+  ))
+
+(defun motd--popup (text)
+  "Show TEXT in the popup frame."
+  (with-current-buffer (get-buffer-create motd--buffer-name)
     (read-only-mode -1)
     (erase-buffer)
     (motd-message-mode t)
     (insert text)
     (insert "\n")
-    (insert "         [ OK ]      \n")
+    (let ((padding (/ (- (frame-width) 40) 2)))
+      (insert (format (format "%%%ds" padding) ""))
+      (insert "[ OK ]")
+      (insert (format (format "%%%ds" padding) ""))
+      (insert "\n"))
     (insert "\n")
     (insert "\n")
+    (setq mode-line-format nil)
+    (when evil-mode
+      (evil-change-state 'emacs))
     (read-only-mode t))
-  (posframe-show motd--posframe-buffer
-                 :position '(1, 1)
-                 :background-color "lightgreen"
-                 :border-color "grey"
-                 ;; :initialize #'motd--posframe-setup-click-callback
-                 :accept-focus t
-                 ;; :timeout 2
-                 :border-width 5
-                 :internal-border-width 10
-                 :poshandler 'posframe-poshandler-frame-center))
+  (setq motd--popup-frame
+        (motd--create-frame motd--buffer-name)))
 
 (defun motd--timeout-handler ()
   "Remind you important things upon the first touch of Emacs in the day."
   (let ((day (format-time-string "%d")))
     (unless (or (equal day motd--today) (not (frame-focus-state)))
       (let ((text (motd--report-content)))
-        (if (posframe-workable-p)
-            (motd--popup-posframe text)
-          (motd--popup-ns text)))
+        (motd--popup text)
+        ;; (motd--popup-ns text)
+        )
       (motd--git-commit)
       (setq motd--today day))))
 
@@ -121,19 +154,22 @@ Recent Submission Deadlines:
   (cancel-timer motd--timer)
   (setq motd--timer nil))
 
-(defun motd--remove-posframe ()
-  "Delete the posframe popup."
+(defun motd--remove-popup ()
+  "Delete the popup frame."
   (interactive)
-  (posframe-delete motd--posframe-buffer)
+  (when motd--popup-frame
+    (delete-frame motd--popup-frame)
+    (setq motd--popup-frame nil)
+    )
   )
 
 (define-minor-mode motd-message-mode
   "Minor mode to show motd message."
   :lighter ""
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "RET") 'motd--remove-posframe)
-            (define-key map (kbd "X") 'motd--remove-posframe)
-            (define-key map (kbd "q") '(lambda () (interactive) (message "HI")))
+            (define-key map (kbd "RET") 'motd--remove-popup)
+            (define-key map (kbd "X") 'motd--remove-popup)
+            (define-key map (kbd "q") #'(lambda () (interactive) (message "HI")))
             map))
 
 (provide 'motd)
