437836a5574eed780c24d5d96cdfc0d814803c74
Mingkai Dong <mk@dong.mk>
2024-05-22 22:12:25 +0800

    update

--- early-init.el
+++ early-init.el
@@ -35,6 +35,11 @@
                                     (getenv "LD_LIBRARY_PATH")))
   (push "/data/data/com.termux/files/usr/bin" exec-path)
   )
+(when (string-equal system-type "android")
+  ;; Add Termux binaries to PATH environment
+  (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
+    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
+    (setq exec-path (append exec-path (list termuxpath)))))
 
 (unless is-android
   (when (display-graphic-p)
