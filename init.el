5c175105f09475d7a46247d0de658bfab1ddf27f
Mingkai Dong <mk@dong.mk>
2024-07-10 15:12:48 +0800

    regularly update config

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;;; Code:
 
 
-(setq gc-cons-threshold (* 400 (expt 2 20))
+(setq gc-cons-threshold (* 4 (expt 2 20))
       gc-cons-percentage 0.6)
 
 (let* (;; (file-name-handler-alist nil)  ; This causes loading issues. Check: https://lists.gnu.org/archive/html/emacs-devel/2022-08/msg00218.html
@@ -24,6 +24,8 @@
 
 (setq gc-cons-threshold (expt 2 23)
       gc-cons-percentage 0.1)
+(setq gc-cons-threshold (expt 2 28)
+      gc-cons-percentage 0.3)
 
 (message "Everything is up. Wish you a nice day. :)")
 (put 'narrow-to-region 'disabled nil)
